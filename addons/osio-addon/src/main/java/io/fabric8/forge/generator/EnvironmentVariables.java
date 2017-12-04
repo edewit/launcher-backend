72b18f7337bfdcec81c20689ed99a4bbc43b34c2
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-12-04 13:41:19 +0100

    moved from https://github.com/fabric8io/fabric8-generator

---
+++ addons/osio-addon/src/main/java/io/fabric8/forge/generator/EnvironmentVariables.java
@@ -0,0 +1,48 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.forge.generator;
+
+import io.fabric8.utils.Strings;
+
+/**
+ */
+public class EnvironmentVariables {
+    public static final String KEYCLOAK_SAAS = "KEYCLOAK_SAAS_URL";
+    public static final String WIT_URL = "WIT_URL";
+    public static final String AUTH_URL = "AUTH_URL";
+    public static final String GOGS_URL = "GOGS_URL";
+    public static final String NAMESPACE = "KUBERNETES_NAMESPACE";
+
+    public static final String TESTING_OAUTH_HEADER = "TESTING_OAUTH_HEADER";
+    public static final String OPENSHIFT_API_URL = "OPENSHIFT_API_URL";
+
+    public static String getWitApiURL() {
+        String witAPI = System.getenv(WIT_URL);
+        if (Strings.isNullOrBlank(witAPI)) {
+            witAPI = "https://api.openshift.io/";
+        }
+        return witAPI;
+    }
+
+    public static String getAuthApiURL() {
+        String authAPI = System.getenv(AUTH_URL);
+        if (Strings.isNullOrBlank(authAPI)) {
+            authAPI = "https://auth.openshift.io/";
+        }
+        return authAPI;
+    }
+}
---
+++ addons/osio-addon/src/main/java/io/fabric8/forge/generator/Fabric8GeneratorInitializer.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2005-2015 Red Hat, Inc.
+ * <p>
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package io.fabric8.forge.generator;
+
+import javax.enterprise.event.Observes;
+
+import io.fabric8.forge.generator.pipeline.JenkinsPipelineLibrary;
+import org.jboss.forge.addon.maven.archetype.ArchetypeCatalogFactoryRegistry;
+import org.jboss.forge.furnace.container.cdi.events.Local;
+import org.jboss.forge.furnace.event.PostStartup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ */
+public class Fabric8GeneratorInitializer {
+    public static final String OBSIDIAN_QUICKSTARTS_CATALOG = "Quickstarts";
+    private static final transient Logger LOG = LoggerFactory.getLogger(Fabric8GeneratorInitializer.class);
+
+    public void onInit(@Observes @Local PostStartup startup, ArchetypeCatalogFactoryRegistry registry, JenkinsPipelineLibrary jenkinsPipelineLibrary) throws Exception {
+        LOG.info("Using Jenkinsfile library at: " + jenkinsPipelineLibrary.getWorkflowFolder());
+
+    }
+}
